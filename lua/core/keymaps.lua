fd18198f5aa9f3bac41befc1424eb8c7ae4873ba
zjl <2739468442@qq.com>
2023-02-28 23:37:58 +0800

    add some shortcuts with <leader> for save, quit...

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,6 +2,14 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+-- 正常模式 --
+-- 快捷键退出
+keymap.set("n", "<leader>q", ":q<CR>")
+-- 快捷键保存
+keymap.set("n", "<leader>w", ":w<CR>")
+-- 快捷键保存退出
+keymap.set("n", "<leader>x", ":x<CR>")
+
 -- 视觉模式 --
 -- 单行/多行移动
 keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
@@ -14,5 +22,5 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 -- bufferline
-keymap.set("n", "<A-h>", ":BufferLineCyclePrev<CR>");
-keymap.set("n", "<A-l>", ":BufferLineCycleNext<CR>");
+keymap.set("n", "<A-h>", ":BufferLineCyclePrev<CR>")
+keymap.set("n", "<A-l>", ":BufferLineCycleNext<CR>")
